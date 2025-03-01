66ec24678f280cd1f4ac422d7fbd28d449a12dd4
henry.zwap <henryloth.zwap@icloud.com>
2025-03-02 05:49:21 +0800

    feat(telescope): add search document symbol

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -7,12 +7,14 @@ vim.keymap.set('n', '<leader>ps', function()
 
 end)
 
+vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, {})
+
 vim.keymap.set('v', '<leader>ps', function()
 	local selected_text = vim.fn.getreg('"')
     if selected_text == '' then
         print("No text selected!")
         return
-    end	
+    end
     -- Use Telescope to grep the selected text
-    builtin.grep_string({ search = selection });
+    builtin.grep_string({ search = selected_text });
 end)
