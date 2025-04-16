fec570952789c639d0fb0d8aa23ec4bd6353490a
henry.zwap <henryloth.zwap@icloud.com>
2025-04-16 12:40:25 +0800

    feat(noice)

---
+++ lua/config/catppuccin.lua
@@ -0,0 +1,13 @@
+require("catppuccin").setup({
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+    }
+})
