b4960c0d54ca472ef7edfb8aa1e3123f5d020872
henryG15 <hingor1227@gmail.com>
2025-04-25 01:07:03 +0800

    feat(macro-recording): status line

--- lua/plugins/status_line.lua
+++ lua/plugins/status_line.lua
@@ -1,4 +1,7 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    dependencies = {
+		'nvim-tree/nvim-web-devicons',
+		'yavorski/lualine-macro-recording.nvim',
+	}
 }
