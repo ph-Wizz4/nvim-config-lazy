5c2aba702cfb19e6ff0d076ed32343395d269eb2
henry.zwap <henryloth.zwap@icloud.com>
2025-05-20 10:20:25 +0800

    fest(surround): add surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -64,5 +64,16 @@ return {
 	},
 
 	-- catppuccin
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	-- surround
+	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
+	},
 }
