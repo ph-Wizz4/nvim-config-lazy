9e76b83cf805a57d14a9a807f61d15f2da909bb8
henry.zwap <henryloth.zwap@icloud.com>
2025-07-21 15:26:00 +0800

    feat(copilot): disable on startup

--- lua/config/copilot.lua
+++ lua/config/copilot.lua
@@ -1,2 +1,5 @@
-vim.api.nvim_set_keymap('n', '<leader>i', "<cmd>CopilotChatToggle<CR>",
-{ noremap= true, silent= false})
+vim.api.nvim_set_keymap('n', '<leader>i', "<cmd>CopilotChatToggle<CR>",{ noremap= true, silent= false})
+vim.api.nvim_set_keymap('n', '<leader>id', "<cmd>Copilot disable<CR>",{ noremap= true, silent= false})
+vim.api.nvim_set_keymap('n', '<leader>ie', "<cmd>Copilot enable<CR>", { noremap= true, silent= false})
+
+vim.cmd("Copilot disable")
