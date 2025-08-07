7716edd1a96b71951d893b7dca07d90226d6560b
henry.zwap <henryloth.zwap@icloud.com>
2025-08-07 09:55:23 +0800

    feat(lsp): diagnostic icons

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -9,3 +9,26 @@ vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.show_line_dia
 vim.api.nvim_set_keymap('n', '<leader>n', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', '<leader>N', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap=true, silent=true })
 
+-- diagnostic
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",  -- Error icon (e.g., a cross)
+      [vim.diagnostic.severity.WARN] = "",   -- Warning icon (e.g., a triangle)
+      [vim.diagnostic.severity.INFO] = "",   -- Info icon (e.g., an 'i' in a circle)
+      [vim.diagnostic.severity.HINT] = "",   -- Hint icon (e.g., a lightbulb)
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+      [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+      [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+      [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticLineError",
+      [vim.diagnostic.severity.WARN] = "DiagnosticLineWarn",
+      [vim.diagnostic.severity.INFO] = "DiagnosticLineInfo",
+      [vim.diagnostic.severity.HINT] = "DiagnosticLineHint",
+    },
+  },
+})
