12eb032f9725f37ab04b4a302f90d4b893fe3d2f
henry.zwap <henryloth.zwap@icloud.com>
2025-08-07 10:10:57 +0800

    feat(telescope): add diagnostic

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -3,7 +3,8 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pq', builtin.quickfix, {})
 vim.keymap.set('n', '<leader>pm', builtin.marks, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 
