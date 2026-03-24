e44267afbbb7711b977e8cf9ce4799682adcac6d
henry.zwap <henryloth.zwap@icloud.com>
2026-03-24 15:33:38 +0800

    refactor(core): add options.lua with vim settings
    Move vim.opt settings from basic.lua and theme.lua into core/options.lua

---
+++ lua/core/options.lua
@@ -0,0 +1,9 @@
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.smarttab = true
+vim.opt.rnu = true
+vim.opt.nu = true
+vim.opt.scrolloff = 99
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.o.wrap = false
