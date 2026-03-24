5837b5b0828ac6fe2660e2d0859a1045e35d87fd
henry.zwap <henryloth.zwap@icloud.com>
2026-03-24 17:18:07 +0800

    chore(refactor): move transparent highlights to theme.lua

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -3,4 +3,9 @@
 local THEME = "cyberdream"
 
 vim.cmd.colorscheme(THEME)
+
+-- transparent
+vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
+vim.cmd([[hi NonText guibg=NONE ctermbg=NONE]])
+vim.cmd([[hi NormalNC guibg=NONE ctermbg=NONE]])
 print(string.format("Activated colorscheme: %s", THEME))
