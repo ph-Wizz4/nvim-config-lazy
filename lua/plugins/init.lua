810169eb6309dc69f7fd5d270a8256d4bb4b7d61
henry.zwap <henryloth.zwap@icloud.com>
2026-03-24 16:33:43 +0800

    refactor(plugins): clean up init.lua, fix pug-lsp bug
    - Remove extracted plugins (markdown, lazygit, ui)
    - Fix pug-lsp config (was calling nvim-surround.setup instead)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,9 @@
+-- Main plugin entry
+-- Groups: general, theme, tools, ui enhancements
+-- See individual plugin files for dependencies:
+--   - markdown.lua (requires treesitter)
+--   - lazygit.lua (requires telescope)
+--   - ui.lua (noice + surround)
 return {
 	-- general
 	{ "folke/todo-comments.nvim", opts = {}, },
@@ -8,72 +14,14 @@ return {
 	{"scottmckendry/cyberdream.nvim", opts = {},},
 	{ "folke/tokyonight.nvim", opts = {},},
 
+	-- tools
 	{'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	requires = { {'nvim-lua/plenary.nvim'} },},
-
-	-- center
 	{ 'smithbm2316/centerpad.nvim' },
 
-	-- md
-	{
-		'MeanderingProgrammer/render-markdown.nvim',
-		after = { 'nvim-treesitter' },
-		requires = { 'echasnovski/mini.nvim', opt = true },
-		config = function()
-			require('render-markdown').setup({})
-		end,
-	},
-
-
-	-- Git and lazy git
-	{
-		"kdheepak/lazygit.nvim",
-		requires = {
-			"nvim-telescope/telescope.nvim",
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			require("telescope").load_extension("lazygit")
-		end,
-	},
-
-
-	-- noice
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		}
-	},
-
-	-- catppuccin
+	-- catppuccin (theme integration)
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	-- surround
-	{
-		"kylechui/nvim-surround",
-		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end
-	},
 
-	-- pug lsp
-	{ "opa-oz/pug-lsp", config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end
-	}
+	-- pug-lsp
+	{ "opa-oz/pug-lsp" },
 }
