def2579c6d5afff08732bf2a419edbf3b672da7a
henry.zwap <henryloth.zwap@icloud.com>
2026-03-24 15:33:46 +0800

    refactor(core): add keymaps.lua with consolidated keymaps
    Move general keymaps from keymap.lua, copilot.lua, gitsign.lua, dap.lua, noice.lua, theme.lua into core/keymaps.lua

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,25 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.api.nvim_set_keymap("n", "<leader>pv", "<cmd>Oil<CR>", {noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('t', '<C-R>', [[<C-\><C-N>"..vim.fn.nr2char(vim.fn.getchar())..'pi']], { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-h>', '<C-\\><C-N><C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-j>', '<C-\\><C-N><C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-k>', '<C-\\><C-N><C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-l>', '<C-\\><C-N><C-w>l', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('i', '<A-h>', '<C-\\><C-N><C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<A-j>', '<C-\\><C-N><C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<A-k>', '<C-\\><C-N><C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<A-l>', '<C-\\><C-N><C-w>l', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<A-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-l>', '<C-w>l', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>c', "<cmd>lua require'centerpad'.toggle{ leftpad = 40, rightpad = 40 }<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>l', '<cmd>Lazy<CR>', {noremap = true, silent = true })
