645f8ac565aee27e8105f09f250f5e52b89cf216
henry.zwap <henryloth.zwap@icloud.com>
2026-03-24 15:34:02 +0800

    refactor(config): remove keymaps from plugin config files
    Keymaps moved to core/keymaps.lua, config files now only contain setup

--- lua/config/gitsign.lua
+++ lua/config/gitsign.lua
@@ -1,7 +1,4 @@
--- Gitsign
 vim.api.nvim_set_keymap('n', '<leader>lb', '<cmd>Gitsigns toggle_current_line_blame<CR>', {noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gn', '<cmd>Gitsigns next_hunk<CR>', {noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gN', '<cmd>Gitsigns prev_hunk<CR>', {noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gN', '<cmd>Gitsigns prev_hunk<CR>', {noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>rh', '<cmd>Gitsigns reset_hunk<CR>', {noremap = true, silent = true })
-
