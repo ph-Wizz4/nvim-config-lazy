40ed87f15b56fdcc491a3d42e69ac761706df916
henry.zwap <henryloth.zwap@icloud.com>
2026-03-24 15:33:53 +0800

    refactor(init): update init.lua to require core modules
    - Add requires for core.options, core.keymaps, core.highlights
    - Simplify theme.lua to only set colorscheme

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,28 +1,4 @@
--- active theme
 local THEME = "cyberdream"
 
 vim.cmd.colorscheme(THEME)
 print(string.format("Activated colorscheme: %s", THEME))
-
--- high light
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='yellow' })
-vim.api.nvim_set_hl(0, 'LineNr', { fg='yellow', bg='blue', bold=true})
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='yellow' })
-
--- visual
-vim.opt.smarttab = true
-vim.opt.rnu = true
-vim.opt.nu = true
-vim.opt.scrolloff = 99
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.o.wrap = false
-
--- transparent --
-vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
-vim.cmd([[hi NonText guibg=NONE ctermbg=NONE]])
-vim.cmd([[hi NormalNC guibg=NONE ctermbg=NONE]])
-
--- center pad --
-vim.api.nvim_set_keymap('n', '<leader>c', "<cmd>lua require'centerpad'.toggle{ leftpad = 40, rightpad = 40 }<cr>", { silent = true, noremap = true })
-
