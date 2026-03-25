f1ef7e89eff589adbaac12cf56284eb3dbd2b12f
henry.zwap <henryloth.zwap@icloud.com>
2026-03-25 14:31:37 +0800

    feat(core/keymap): use Oil floating buffer

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.api.nvim_set_keymap("n", "<leader>pv", "<cmd>Oil<CR>", {noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>pv", "<cmd>Oil --float<CR>", {noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('t', '<C-R>', [[<C-\><C-N>"..vim.fn.nr2char(vim.fn.getchar())..'pi']], { noremap = true, expr = true, silent = true })
 vim.api.nvim_set_keymap('t', '<A-h>', '<C-\\><C-N><C-w>h', { noremap = true, silent = true })
