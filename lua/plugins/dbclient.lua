5c878ebb65a42212e095fc9f83e567f518213cd6
henry.zwap <henryloth.zwap@icloud.com>
2026-03-26 11:53:16 +0800

    remove vim-dadbod plugin

--- lua/plugins/dbclient.lua
+++
@@ -1,7 +0,0 @@
--- vim-dadbod + vim-dadbod-ui: Database client
--- Related: config/ (no specific config file)
-return {
-  "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-completion",
-  "kristijanhusak/vim-dadbod-ui",
-}
