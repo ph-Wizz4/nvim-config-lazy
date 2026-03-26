87019ac2cbce6af71ad34692550d08cfd73392b8
henry.zwap <henryloth.zwap@icloud.com>
2026-03-26 12:35:45 +0800

    refactor(highlights): use nvim_set_hl consistently

--- lua/core/highlights.lua
+++ lua/core/highlights.lua
@@ -1,10 +1,10 @@
 return function ()
-		vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#e63380' })
-		vim.api.nvim_set_hl(0, 'CursorLineNr', { fg='#33cce6', bold=true})
-		vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#fbda1a' })
-		-- transparent bg
-		vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
-		vim.cmd([[hi NonText guibg=NONE ctermbg=NONE]])
-		vim.cmd([[hi NormalNC guibg=NONE ctermbg=NONE]])
-	end
+	vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#e63380' })
+	vim.api.nvim_set_hl(0, 'CursorLineNr', { fg='#33cce6', bold=true})
+	vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#fbda1a' })
+	vim.api.nvim_set_hl(0, "FloatBorder", { fg='#33cce6' })
+	vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
+	vim.api.nvim_set_hl(0, 'NonText', { bg = 'NONE' })
+	vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'NONE' })
+end
 
