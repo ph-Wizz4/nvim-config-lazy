bfd20d983d235b228b9be6e99a6d69292783df80
henry.zwap <henryloth.zwap@icloud.com>
2026-05-05 02:00:14 +0800

    feat(hightlight): use number hight

--- lua/core/highlights.lua
+++ lua/core/highlights.lua
@@ -1,6 +1,7 @@
 return function ()
 	vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#e63380' })
 	vim.api.nvim_set_hl(0, 'CursorLineNr', { fg='#33cce6', bold=true})
+	vim.api.nvim_set_hl(0, 'LineNr', { fg='yellow', bg='blue' })
 	vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#fbda1a' })
 	vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
 	vim.api.nvim_set_hl(0, 'NonText', { bg = 'NONE' })
