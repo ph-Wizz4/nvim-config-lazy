ca166cd8e06b4a8681e82b1950d8bd77286cf834
HenryLO <henrylo@HenryLOs-MacBook-Air.local>
2026-08-25 21:51:22 +0800

    feat(toggleterm): allow ctrl-r in terminal

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -12,5 +12,16 @@ require("toggleterm").setup({
         border = 'curved',     -- Options: 'single', 'double', 'shadow', 'curved', etc.
         winblend = 3,         -- Transparency of the terminal window
 		Transparency = 0.1,
-    },
+	},
+	on_open = function(term)
+		-- Ensure C-r passes through directly to the shell inside toggleterm buffers
+		vim.keymap.set('t', '<C-r>', function()
+			vim.api.nvim_feedkeys(
+				vim.api.nvim_replace_termcodes('<C-r>', true, false, true),
+				'n',
+				false
+			)
+		end, { buffer = term.bufnr, noremap = true, silent = true })
+	end,
 })
+
